f0dcbaa2f7a9e672764e1a7ce4f64d948e3ccc54
Alex Brik <g00dv1n.private@gmail.com>
2017-05-11 14:01:01 +0300

    add body parser

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import * as Koa from 'koa';
 import * as logger from 'koa-logger';
+import * as bodyParser from 'koa-bodyparser';
 import * as config from 'config';
 import * as chalk from 'chalk';
 
 const app: Koa = new Koa();
 
 app.use(logger());
+app.use(bodyParser());
 
 app.use((ctx: Koa.Context) => {
   ctx.body = 'Koa with TS!';
