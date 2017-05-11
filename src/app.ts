60fac7684557e190b045081d05548af749cb4ba5
Alex Brik <g00dv1n.private@gmail.com>
2017-05-11 14:00:06 +0300

    add config

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import * as Koa from 'koa';
+import * as logger from 'koa-logger';
+import * as config from 'config';
+import * as chalk from 'chalk';
+
+const app: Koa = new Koa();
+
+app.use(logger());
+
+app.use((ctx: Koa.Context) => {
+  ctx.body = 'Koa with TS!';
+});
+
+const listen = () => {
+  const port = config.get('PORT');
+  console.log(chalk.green.bgBlack.bold(`app listenning on ${port} port...`))
+  app.listen(port);
+};
+
+listen();
